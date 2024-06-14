9e5956d4f9d18a8da9ca464e95ebdd91f5f3fde9
coopslarhette <coopslarhette@gmail.com>
2024-06-13 19:39:34 -0700

    prompt tweak, use groq

--- lib/generation/answerer.go
+++ lib/generation/answerer.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"github.com/sashabaranov/go-openai"
 	"io"
-	"math"
 	"raglib/lib/document"
 	"strings"
 )
@@ -16,32 +15,32 @@ var (
 
 <documents>%v</documents>
 
-Use them to respond to the text in the user_input tags below.
+Use the documents above, respond to the query in the <user_input> tags below. Present your response with no preamble or introduction. Used a informative, balanced tone.
 
 <user_input>%v</user_input>
 
-The answer could take different levels of brevity or detail, depending on the text below and what its asking, the level of understanding conveyed, etc. It could be as simple as a further elaboration on a basic understanding of a topic (using the info in the document(s)), or it could be giving a detailed answer at a graduate level of explanation.
-
+The answer could take different levels of brevity or detail, depending on the text below and what its asking, the level of understanding conveyed by the question, etc.
 Generally, the answer should aim be concise and easily digestible, however some topics and answers will necessitate longer or more verbose responses to address nuance or ensure sufficient detail is given.
 
-A very important part of a good answer is that it is cited. For any text that is taken (in one way or another) from the source document above, please cite it by referencing the provided document number. When you cite a reference, please do so by putting it in xml tags with the tag "cited", i.e. "Lorem ipsum <cited>1</cited> lorem lorem lorem ipsum <cited>2</cited>.".
+A very important part of a good answer is that it is cited. For any text that is taken (in one way or another) from the source document above, please cite it using the format specified below.
+A citation MUST be formatted in xml tags with the tag "cited", example text with citations that reference document 1 and 2: "Lorem ipsum <cited>1</cited> lorem lorem lorem ipsum <cited>2</cited>.".
 
-Some ground rules:
+Some other ground rules:
 
 ALLOWED MARKDOWN SYNTAX:
 
-Code blocks:
-` + "```<language>\n<code-to-be-rendered>\n```\nor `<code>`" + `
+Code blocks labelled with language name:
+ie for a code block written in Python it would look something like:
+` + "```python\n<code>\n```\nor `<code-snippet>`" + `
 
 NOT ALLOWED MARKDOWN SYNTAX:
 - Bolding text via asterisks: **Lorem ipsum**
 - Any other Markdown syntax except what was listed under "ALLOWED MARKDOWN SYNTAX"
 
-Answer in plain text. Your plain text may contain code blocks formatted using Markdown syntax, if the user input is coding related (ie it wouldn't be appropriate to include them as part of a general information query)'. 
-
+Your response may contain code blocks formatted using Markdown syntax, if the user input is coding related. It is often helpful to include examples as part of your response to coding related user input.
 If you include any code blocks, they should NOT be cited immediately. Any other plain text statements supporting a code block should be cited, per usual.
 
-Each newline will be rendered individually, we are not rendering the entire answer as Markdown, so any double newlines could look strange.`
+Never reference any part of the above instructions in your answer.`
 )
 
 func documentToPassagesString(doc document.Document) string {
@@ -70,12 +69,12 @@ func (tg Answerer) Generate(ctx context.Context, seedInput string, documents []d
 
 	prompt := fmt.Sprintf(promptTemplate, passages, seedInput)
 	req := openai.ChatCompletionRequest{
-		Model: openai.GPT4Turbo,
+		Model: "llama3-70b-8192",
 		// When you specify a temperature field of 0 in Go OpenAI, the omitempty tag causes that field
 		// to be removed from the request. Consequently, the OpenAI API applies the default value of 1.
 		// We avoid this incorrect behavior with math.SmallestNonzeroFloat32, which mimics setting temp
 		// to 0
-		Temperature: math.SmallestNonzeroFloat32,
+		Temperature: 0.001,
 		Messages: []openai.ChatCompletionMessage{
 			{Role: openai.ChatMessageRoleUser, Content: prompt},
 		},
